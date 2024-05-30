f227ccfeee615419c5e4bb2ccb67989721cc218b
SenneDG <senne.degreef@student.kuleuven.be>
2024-05-30 15:15:23 +0200

    reload

--- target/classes/static/dsgt-frontend/src/store/store.ts
+++ target/classes/static/dsgt-frontend/src/store/store.ts
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
+import { loadState, saveState } from './../utils/helpers/LocalStorage'
 
 declare global {
   interface Window {
@@ -7,10 +8,22 @@ declare global {
   }
 }
 
-const middleware = applyMiddleware();
+const persistedState = loadState();
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(middleware));
+const store = createStore(
+  rootReducer,
+  persistedState, // Load persisted state here
+  composeEnhancers(applyMiddleware())
+);
+
+// Subscribe to store updates to save state to localStorage
+store.subscribe(() => {
+  saveState({
+    user: store.getState().user,
+  });
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 
